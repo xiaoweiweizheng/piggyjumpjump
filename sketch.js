f433f584aa98fb3bc2a59e7d7c7ee22b0dc578ee
xiaoweiweizheng <xiaoweiweizheng@gmail.com>
2019-11-19 17:30:54 -0800

    update pseudocode

--- sketch.js
+++ sketch.js
@@ -1,7 +1,36 @@
+// pseudocode
+// you can stick to basic code outline if that helps give you structure
+
+
+// declare variables for:
+// game state
+// points
+
 function setup() {
   createCanvas(400, 400);
+
 }
 
 function draw() {
-  background(220);
+
+  //   if game state variable is "title"
+  //   then draw the title page
+  // (execute title page function)
+
+  //   if game state is "first level"
+  //   execute first level function
+
+  // if points drop to below 0
+  // execute game over function
+
+  // if points exceeds 100
+  // execute you won function
+}
+
+function displayTitlePage(){
+
+  // make background black
+  // display image of my sprite
+  // display text "my fantastic game"
+
 }
